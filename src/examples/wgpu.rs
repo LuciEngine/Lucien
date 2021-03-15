0b27f6c0e27e8b7c61163ca9d1d7de4a03b97b3c
knh190 <knh190@hotmail.com>
2021-03-15 02:47:13 -0400

    render state.

--- src/examples/wgpu.rs
+++ src/examples/wgpu.rs
@@ -1,12 +1,9 @@
-use crate::render::buffer::render_buffer;
 use crate::render::*;
 
 use anyhow::{Context, Result};
 use futures::executor::block_on;
 
-async fn init_gpu_headless(
-    size: &[u32; 2],
-) -> Result<(wgpu::Device, wgpu::Queue, RenderTexture, wgpu::Buffer)> {
+async fn init_gpu_headless() -> Result<(wgpu::Device, wgpu::Queue)> {
     let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
     let adapter = instance
         .request_adapter(&wgpu::RequestAdapterOptions {
@@ -19,22 +16,18 @@ async fn init_gpu_headless(
         .request_device(&Default::default(), None)
         .await
         .context("Failed to request device")?;
-    let rt =
-        RenderTexture::new(size[0], size[1], &device).context("Failed to create render texture")?;
-    let rb = render_buffer(&rt, &device);
 
-    Ok((device, queue, rt, rb))
+    Ok((device, queue))
 }
 
-// Same thing for iced ui, all we need is state.render()
 pub fn main() {
     let size = [256, 256];
-    let (device, queue, rt, rb) = block_on(init_gpu_headless(&size)).unwrap();
+    let (device, queue) = block_on(init_gpu_headless()).unwrap();
 
-    let render_settings = RenderSettings::new(rt);
-    let mut renderer = Renderer::new(device, queue, size, Some(rb)).unwrap();
+    let render_settings = RenderSettings::new();
+    let mut renderer = Renderer::new(device, queue, size).unwrap();
 
     renderer.render(&render_settings).unwrap();
     renderer.update();
-    renderer.read_to_buffer(&render_settings).unwrap();
+    renderer.read_to_buffer().unwrap();
 }
--- src/render/renderer.rs
+++ src/render/renderer.rs
@@ -4,15 +4,14 @@ use wgpu;
 use crate::render::*;
 
 // Render to render_texture, and you can read it to render_buffer
+#[derive(Debug, Copy, Clone)]
 pub struct RenderSettings {
-    pub render_texture: RenderTexture,
     pub render_mode: RenderMode,
     pub clear_color: Option<wgpu::Color>,
 }
-// Renderer takes a RenderSettings, writes data to a RenderState
+// Renderer accepts a RenderSettings, writes data to a RenderState
 // rt is a render texture (render target)
 // rb is a render buffer, can read data from the texture
-#[allow(dead_code)]
 pub struct RenderState {
     pub rt: RenderTexture,
     pub rb: Option<wgpu::Buffer>,
@@ -29,32 +28,21 @@ pub struct Renderer {
     queue: wgpu::Queue,
     textured_pipeline: wgpu::RenderPipeline,
     wireframe_pipeline: wgpu::RenderPipeline,
-    uniforms: Uniforms,
-    scene: Scene,
-    depth_texture: DepthTexture,
-    render_buffer: Option<wgpu::Buffer>,
-    // state: RenderState,
+    state: RenderState,
 }
 
 impl Renderer {
-    pub fn new(
-        device: wgpu::Device, queue: wgpu::Queue, size: [u32; 2], rb: Option<wgpu::Buffer>,
-    ) -> Result<Self> {
-        // set up scene
-        let scene = Scene::new(&device).load("src/examples/data/cube.obj", &device, &queue);
-        // create buffer
-        let uniforms = Uniforms::new(&scene, &device);
-        // create depth texture
-        let depth_texture = DepthTexture::new(&device, size[0], size[1], Some("depth_texture"));
-
+    pub fn new(device: wgpu::Device, queue: wgpu::Queue, size: [u32; 2]) -> Result<Self> {
+        let state =
+            RenderState::new(size, &device, &queue).context("Failed to create render state")?;
         // first model & material
-        let mesh = &scene.models[0].mesh;
-        let material = &scene.materials[mesh.material];
+        let mesh = &state.scene.models[0].mesh;
+        let material = &state.scene.materials[mesh.material];
 
-        let mut bind_group_layouts = vec![&uniforms.bind_group_layout];
+        let mut bind_group_layouts = vec![&state.uniforms.bind_group_layout];
         bind_group_layouts.push(&material.diffuse_texture.layout);
         bind_group_layouts.push(&material.bind_group_layout);
-        bind_group_layouts.push(&scene.light.bind_group_layout);
+        bind_group_layouts.push(&state.scene.light.bind_group_layout);
 
         // render pipeline
         let render_pipeline_layout =
@@ -72,60 +60,61 @@ impl Renderer {
             queue,
             textured_pipeline,
             wireframe_pipeline,
-            uniforms,
-            scene,
-            depth_texture,
-            render_buffer: rb,
+            state,
         })
     }
 
     pub fn update(&mut self) {
         let mut encoder = Renderer::create_encoder(Some("Update Encoder"), &self.device);
         // update scene
-        self.scene.camera.eye.z -= 0.01;
-        self.scene.camera.update_view_matrix();
-        self.scene.light.position = self.scene.camera.eye;
+        self.state.scene.camera.eye.z -= 0.01;
+        self.state.scene.camera.update_view_matrix();
+        self.state.scene.light.position = self.state.scene.camera.eye;
 
-        self.uniforms
-            .update_buffer(&self.scene, &mut encoder, &self.device);
-        self.scene.light.update_buffer(&mut encoder, &self.device);
+        self.state
+            .uniforms
+            .update_buffer(&self.state.scene, &mut encoder, &self.device);
+        self.state
+            .scene
+            .light
+            .update_buffer(&mut encoder, &self.device);
 
         self.queue.submit(std::iter::once(encoder.finish()));
     }
 
-    pub fn render(&mut self, settings: &RenderSettings) -> Result<(), wgpu::SwapChainError> {
+    pub fn render(&mut self, settings: &RenderSettings) -> Result<()> {
         let clear = settings.get_clear_color();
         let mut encoder = Renderer::create_encoder(Some("Render Encoder"), &self.device);
         {
-        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            // write colors to render target
-            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
-                attachment: &settings.render_texture.view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(clear),
-                    store: true,
-                },
-            }],
-            // write z-values to depth texture
-            depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachmentDescriptor {
-                attachment: &self.depth_texture.view,
-                depth_ops: Some(wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(1.0),
-                    store: true,
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                // write colors to render target
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &self.state.rt.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(clear),
+                        store: true,
+                    },
+                }],
+                // write z-values to depth texture
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachmentDescriptor {
+                    attachment: &self.state.depth.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: true,
+                    }),
+                    stencil_ops: None,
                 }),
-                stencil_ops: None,
-            }),
-        });
+            });
             // render first material for first mesh
-            let mesh = &self.scene.models[0].mesh;
-            let material = &self.scene.materials[mesh.material];
+            let mesh = &self.state.scene.models[0].mesh;
+            let material = &self.state.scene.materials[mesh.material];
 
             render_pass.set_pipeline(&self.textured_pipeline);
-            render_pass.set_bind_group(0, &self.uniforms.bind_group, &[]);
+            render_pass.set_bind_group(0, &self.state.uniforms.bind_group, &[]);
             render_pass.set_bind_group(1, &material.diffuse_texture.group, &[]);
             render_pass.set_bind_group(2, &material.bind_group, &[]);
-            render_pass.set_bind_group(3, &self.scene.light.bind_group, &[]);
+            render_pass.set_bind_group(3, &self.state.scene.light.bind_group, &[]);
             render_pass.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
             render_pass.set_index_buffer(mesh.index_buffer.slice(..));
             render_pass.draw_indexed(0..mesh.num_indices, 0, 0..1);
@@ -138,9 +127,9 @@ impl Renderer {
         device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label })
     }
 
-    pub fn read_to_buffer(&self, settings: &RenderSettings) -> Result<()> {
+    pub fn read_to_buffer(&self) -> Result<()> {
         assert_eq!(
-            self.render_buffer.is_none(),
+            self.state.rb.is_none(),
             false,
             "Render Buffer not set, can't read render result to buffer."
         );
@@ -148,19 +137,19 @@ impl Renderer {
         let mut encoder = Renderer::create_encoder(Some("Render Buffer Encoder"), &self.device);
         encoder.copy_texture_to_buffer(
             wgpu::TextureCopyView {
-                texture: &settings.render_texture.texture,
+                texture: &self.state.rt.texture,
                 mip_level: 0,
                 origin: wgpu::Origin3d::ZERO,
             },
             wgpu::BufferCopyView {
-                buffer: &self.render_buffer.as_ref().unwrap(),
+                buffer: &self.state.rb.as_ref().unwrap(),
                 layout: wgpu::TextureDataLayout {
                     offset: 0,
-                    bytes_per_row: u32_size * settings.render_texture.size.width,
-                    rows_per_image: settings.render_texture.size.height,
+                    bytes_per_row: u32_size * self.state.rt.size.width,
+                    rows_per_image: self.state.rt.size.height,
                 },
             },
-            settings.render_texture.size,
+            self.state.rt.size,
         );
         self.queue.submit(Some(encoder.finish()));
 
@@ -169,9 +158,8 @@ impl Renderer {
 }
 
 impl RenderSettings {
-    pub fn new(render_texture: RenderTexture) -> Self {
+    pub fn new() -> Self {
         Self {
-            render_texture,
             render_mode: RenderMode::Default,
             clear_color: None,
         }
